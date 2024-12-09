add7a43e79170401cab606522c9070277f87e351
Monika H J <monikahj7@gmail.com>
2024-12-09 16:22:52 +0530

    Making Home PAge

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,8 +7,8 @@ const Home = () => {
     <section className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-52 lg:gap-96 px-4 md:px-5 lg:px-32 py-12 bg-gray-800 text-white ">
       {/* Left Side (Text Content) */}
       <div className="w-full md:w-1/2 text-2xl   ">
-      <p className="text-7xl font-semibold mb-4 ">Hi, I'm </p>
-      <p className="text-7xl font-semibold mb-4  mt-8"> Monika H J</p>
+      <p className="text-7xl font-semibold mb-4 text-fuchsia-400 ">Hi, I'm </p>
+      <p className="text-7xl font-semibold mb-4 text-fuchsia-400  mt-8"> Monika H J</p>
         <p className="text-xl mb-6 mt-5">I'm a Full Stack Developer passionate about building modern web applications.</p>
         <button className="bg-fuchsia-700 text-white px-6 py-2 rounded-lg hover:bg-fuchsia-900">
           Contact Me
