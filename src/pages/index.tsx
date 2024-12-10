c35b31e229189f7f48614ae83cc4481108df7b21
Monika H J <monikahj7@gmail.com>
2024-12-10 18:22:03 +0530

    About Page is Complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Home from "../components/Home"
+import About from "../components/About"
 
 const App = () => {
   return (
     <div className=' bg-gray-800 '>
       <Navbar />
       <Home/>
+      <About/>
     </div>
   );
 };
