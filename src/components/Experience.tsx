c9b39b8066646a35a597a2eed7ca57a381377921
Monika H J <monikahj7@gmail.com>
2025-04-22 13:44:16 +0530

    expreince

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -38,9 +38,57 @@ const Experience = () => {
 
 
 
+
+
                 </div>
 
+
+
             </div>
+
+
+            <div className='mt-4 data-aos="fade-up'>
+                <div className='grid grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 '>
+                    <div>
+                    </div>
+
+                    <div>
+
+                        <p className='text-base text-slate-400 mt-2'>Full Stack Developer</p>
+                        <div className='flex gap-3'>
+                            <p className='text-base text-slate-400 mt-1'>Project 2 : Refer And Earn  </p>
+                            <a
+                                href="https://www.joboxhire.co/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="underline text-slate-400 mt-1"
+                            >
+                                Link
+                            </a>
+                        </div>
+
+                        <p className=" text-base text-slate-400 mt-1"> <li>Developed JOBOX, a job platform, using the T3 Stack (TypeScript, Tailwind CSS, tRPC).</li>
+                            <li>Built responsive front-end interfaces with React.js, Next.js, HTML, CSS, and Tailwind CSS.</li>
+                            <li>Created server-side functionalities with Node.js and tRPC.</li>
+                            <li>Managed PostgreSQL databases and used Prisma ORM.</li>
+                            <li>Handled version control and PR management on GitHub.</li></p>
+
+
+                    </div>
+
+
+
+
+
+                </div>
+
+
+
+            </div>
+
+            <hr className='text-slate-400  mt-8'  />
+
+
 
 
         </div>
